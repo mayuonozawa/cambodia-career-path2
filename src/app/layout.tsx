a8e91ccb807ca288f2107543df2ff966c44aa045
mayuonozawa <mayuonozawa.taylors@gmail.com>
2026-02-12 14:57:55 +0900

    GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,29 @@
+import Script from 'next/script'
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <html lang="ja">
+      <body>
+        {children}
+
+        {/* --- Google Analytics 設置 --- */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-R95RXEFDYX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R95RXEFDYX');
+          `}
+        </Script>
+      </body>
+    </html>
+  );
 }
